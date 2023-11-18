ad762505a4acd744f7eecff3be58198b89be4047
IT Programmer <reinhard.silaen1@gmail.com>
2023-11-18 14:04:07 +0700

    add header and framer motion

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Test from "./Test";
+import "./App.scss"
+import Navbar from "./components/navbar/Navbar";
+
+const App = () => {
+  return <div>
+    {/* <section>
+      <Navbar/>
+      </section> */}
+    {/* <section>Paralax</section>
+    <section>Services</section>
+    <section>Portfolio</section>
+    <section>Portfolio2</section>
+    <section>Portfolio</section>
+    <section>Contact</section> */}
+    <Test/>
+  </div>;
+};
+
+export default App;
