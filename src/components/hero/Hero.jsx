87a11401782e7cf1105254b5b53456b679dbd820
IT Programmer <reinhard.silaen1@gmail.com>
2023-11-23 15:19:43 +0700

    Add animated text

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -3,23 +3,61 @@ import "./hero.scss";
 import {motion} from "framer-motion";
 
 
+const textVariants ={
+  initial : {
+    x : -500,
+    opacity : 0,
+  },
+  animate : {
+    x : 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      stagerChildren: 0.1,
+    },
+  },
+  scrollButton : {
+    opacity: 0, 
+    y: 10,
+    transition:{
+      duration: 2,
+      repeat: Infinity,
+    }
+  }
+}
+
+
+const sliderVariants ={
+  initial : {
+    x : 0,
+  },
+  animate : {
+    x : "-220%",
+    transition: {
+      duration: 20,
+      repeat: Infinity,
+      repeatType: "mirror",
+    },
+  },
+}
+
 const Hero = () => {
   return (
     
     <div className='hero'>
       <div className="wrapper">
-        <div className="text-container" > 
-          <h2>Reinhard Silaen</h2>
-          <h1>Fullstack Developer</h1> 
+        <motion.div className="text-container" variants={textVariants} initial="initial" animate = "animate"> 
+          <motion.h2 variants={textVariants}>Reinhard Silaen</motion.h2>
+          <motion.h1 variants={textVariants}>Fullstack Developer</motion.h1> 
           <div className="buttons">
-            <button >See the latest work</button>
-            <button >Contact Me</button>
+            <motion.button variants={textVariants}>See the latest work</motion.button>
+            <motion.button variants={textVariants}>Contact Me</motion.button>
           </div>
-          <img src="./scroll.png" alt="" />
-        </div>
-        <div className='slidingTextContainer'>
+          <motion.img variants={textVariants} src="./scroll.png" animate= "scrollButton" alt="" />
+        </motion.div>
+        <motion.div className='slidingTextContainer' variants={sliderVariants} initial="initial" animate = "animate">
           ReactJS NEXTJS Laravel
-        </div>
+        </motion.div>
 
       </div>
     </div>
